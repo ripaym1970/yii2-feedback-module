6028c462373cdcc834f73327f6f68b52bbab707a
Yurii <ripaym1970@gmail.com>
2019-08-01 17:07:37 +0300

    Fix send.js i18N

--- src/migrations/m180408_070030_create_feedback_table.php
+++ src/migrations/m180408_070030_create_feedback_table.php
@@ -7,26 +7,42 @@ use yii\db\Migration;
  */
 class m180408_070030_create_feedback_table extends Migration {
 
+    public $newTableName = 'feedback';
+
     /**
      * {@inheritdoc}
      */
     public function safeUp() {
-        $this->createTable('{{%feedback}}', [
-            'id'           => $this->primaryKey(),
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+            //ALTER TABLE feedback CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+        }
+
+        $newTableName = $this->newTableName;
+        if ($this->db->driverName === 'pgsql') {
+            $newTableName = Yii::$app->params['schema'] . '.' . $newTableName;
+        }
+
+        $this->createTable($newTableName, [
+            'id' => $this->primaryKey(),
+
             'name'         => $this->string(),
             'company_name' => $this->string(),
             'tel'          => $this->string(),
             'email'        => $this->string(100),
             'message'      => 'MEDIUMTEXT',
+            'status'       => $this->tinyInteger()->defaultValue(0),
             'created_date' => $this->integer(),
-            'status'       => $this->tinyInteger()->defaultValue(0)
-        ]);
+        ], $tableOptions);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function safeDown() {
-        $this->dropTable('{{%feedback}}');
+        $newTableName = $this->newTableName;
+        if ($this->db->driverName === 'pgsql') {
+            $newTableName = Yii::$app->params['schema'] . '.'. $newTableName;
+        }
+
+        $this->dropTable($newTableName);
     }
 }
